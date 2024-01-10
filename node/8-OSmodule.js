8f78f3704610e714f8817252185b9a08f0bd93fe
Shehzab <106922099+shehzab@users.noreply.github.com>
2024-01-10 19:32:52 +0530

    Create 8-OSmodule.js

---
+++ node/8-OSmodule.js
@@ -0,0 +1,27 @@
+// OS module
+
+const os = require('os')
+
+// info about current user 
+
+const user = os.userInfo()
+
+console.log(user)
+
+// method returns the system suptime in seconds
+console.log(`the System uptime is ${os.uptime()} seconds`)
+
+// info about cpu 
+
+// const cpu = os.cpus();
+// console.log(cpu);
+
+
+const currentOS ={
+    name: os.type(),
+    release:os.release(),
+    totalMem:os.totalmem(),
+   freeMem:os.freemem(),
+}
+
+console.log(currentOS)
