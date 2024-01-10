8dd469e21ba3ab5d9652e39768244f85f998ab72
Shehzab <106922099+shehzab@users.noreply.github.com>
2024-01-10 10:53:50 +0530

    Update app.js

--- node/app.js
+++ node/app.js
@@ -1,1 +1,14 @@
-const names = require ('4-names.js');
+//  Modules can be loaded into program by using the required function
+const names = require('./4-names.js')
+
+console.log(names)
+
+const sayHi = require('./5-utils.js')
+
+console.log(sayHi)
+
+sayHi('susan')
+
+sayHi(names.john)
+
+sayHi(names.peter)
