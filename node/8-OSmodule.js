27a5aa543af8b6684c0bb3ae983ef6c982b44942
Shehzab <106922099+shehzab@users.noreply.github.com>
2024-01-10 19:37:34 +0530

    Update 8-OSmodule.js

--- node/8-OSmodule.js
+++ node/8-OSmodule.js
@@ -25,3 +25,19 @@ const currentOS ={
 }
 
 console.log(currentOS)
+
+
+const os = require('os');
+
+console.log('Architecture:', os.arch());
+console.log('Platform:', os.platform());
+console.log('CPUs:', os.cpus());
+console.log('Total Memory:', os.totalmem() / (1024 * 1024) + ' MB');
+console.log('Free Memory:', os.freemem() / (1024 * 1024) + ' MB');
+console.log('Load Average:', os.loadavg());
+console.log('User Info:', os.userInfo());
+console.log('Home Directory:', os.homedir());
+console.log('Temp Directory:', os.tmpdir());
+console.log('Hostname:', os.hostname());
+
+
